26ca21a94d8898bcaa0dc30cb7a7384844c9f259
Eren Boran <erenn.borann@gmail.com>
2022-04-02 17:19:16 +0300

    MainGame

---
+++ Assets/Scripts/DartRotation.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartRotation : MonoBehaviour
+{
+
+    float degreesPerSecond = 20;
+    public GameObject arrow;
+    public Transform ArrowFirstLoc;
+    public Rigidbody2D rb;
+
+    private void Awake()
+    {
+       // rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, degreesPerSecond) * Time.deltaTime);
+
+        if (Input.GetMouseButton(0))
+        {
+            rb.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (gameObject.tag == "Log")
+        {
+            // rb.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            rb.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+
+            arrow.transform.position = ArrowFirstLoc.transform.position;
+            StartCoroutine(Test());
+            Debug.Log("sadas");
+
+            //if (Input.GetMouseButton(0))
+            //{
+            //    rb.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            //}
+
+
+        }
+        if (gameObject.tag == "Knife")
+        {
+            // rb.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            Debug.Log("tgest");
+
+        }
+        IEnumerator Test()
+        {
+
+            yield return new WaitForSeconds(232);
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject yanlis;
+    public Rigidbody2D yanlis2;
+    private Vector3 kontrol  = new Vector3(0f, -3.51f, 0f);
+
+    void Start()
+    {
+        yanlis.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnTriggerEnter2D (Collider2D other)
+    {
+        
+          
+            if(gameObject.tag == "restart")
+            {
+                SceneManager.LoadScene("SampleScene");
+
+            Destroy(gameObject);
+        }
+            else if(gameObject.tag == "NextLevel")
+        {
+            SceneManager.LoadScene("1");
+        }
+        else if (gameObject.tag == "Log")
+        {
+           
+           
+            yanlis2.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            if (yanlis.transform.position == kontrol)
+            {
+                yanlis2.constraints = RigidbodyConstraints2D.FreezePositionY;
+            }
+        }
+
+
+    }
+}
+
